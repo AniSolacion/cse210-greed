eaf2818b5ef9a635bf0eb2a10956c414a0dc2d30
Logan Huston <shadows7590@gmail.com>
2022-06-06 10:39:09 -0600

    Added base code to program.cs

---
+++ Game/Casting/Gem.cs
@@ -0,0 +1,18 @@
+using System;
+
+namepsace Unit04.Game.Casting
+{
+    public class Gem : Actor
+    {
+        private int score = 0;
+
+        public Gem()
+        {
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+    }
+}
